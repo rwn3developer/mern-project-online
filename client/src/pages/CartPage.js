6de3298ef24d6f623986513ac711f064086c8c75
milansir <rwn3developer11@gmail.com>
2024-01-02 08:38:44 +0530

    profile-update-task done

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -87,6 +87,29 @@ const CartPage = () => {
                            <hr/>
                            <h4>Total : - {totalPrice()}</h4>
                         </div>
+
+                        {
+                            auth?.user?.address ? (
+                                <>
+                                    <div className='mb-3'>
+                                        <h4>Current Address</h4>
+                                        <h5>{auth?.user?.address}</h5>
+                                        <button className='btn btn-outline-warning' onClick={ () => navigate(`/dashboard/user/profile`) }>Update Address</button>
+                                    </div>
+                                </>
+                            ) : (
+                                <div className='mb-3'>
+                                    {
+                                        auth?.token ? (
+                                            <button className='btn btn-outline-warning' onClick={ () => navigate(`/dashboard/user/profile`) }>Update Address</button>
+                                        ) : (
+                                            <button className='btn btn-outline-warning' onClick={ () => navigate('/login',{state : '/cart'}) }>Please Login to checkout</button>
+                                        )
+                                    }
+                                </div>
+                            )
+                        }
+
                     </div>
                 </div>
 
